9271145021fb865f58da6aa7c49a53b6ff2b7fcd
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2023-07-27 15:59:07 -0700

    make sure nvim uses correct python

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -9,13 +9,12 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
-
+-- make sure nvim can access the correct python
+vim.g.python3_host_prog = "/usr/bin/python3"
 
 -- nvim-tree says to do this
-
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.opt.termguicolors = true
-
 ----------------------------
