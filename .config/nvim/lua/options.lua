ec62063931faf221b42c1235df5881177a014474
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2023-07-29 16:09:32 -0600

    .pl files are prolog (not perl)

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -9,6 +9,9 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+-- fix filetype settings
+vim.g.filetype_pl = "prolog"
+
 -- make sure nvim can access the correct python
 vim.g.python3_host_prog = "/usr/bin/python3"
 
