61125130873737544b828e15263bcf95196c50d6
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2023-07-31 14:05:26 -0700

    ignore .git in telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,22 +1,22 @@
 return {
-    "nvim-telescope/telescope.nvim", tag = "0.1.2",
+    "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local builtin = require("telescope.builtin")
 
-        -- find files 
-        vim.keymap.set("n", "<leader>ff", function ()
-            builtin.find_files{hidden=true}
+        -- find files
+        vim.keymap.set("n", "<leader>ff", function()
+            builtin.find_files { hidden = true }
         end)
 
-        -- find files (including ignored) 
-        vim.keymap.set("n", "<leader>fa", function ()
-            builtin.find_files{hidden=true, no_ignore=true}
+        -- find files (including ignored)
+        vim.keymap.set("n", "<leader>fa", function()
+            builtin.find_files { hidden = true, no_ignore = true }
         end)
 
         -- search text
-        vim.keymap.set("n", "<leader>fg", function ()
-            builtin.live_grep{hidden=true}
+        vim.keymap.set("n", "<leader>fg", function()
+            builtin.live_grep { hidden = true }
         end)
 
         -- search buffers
@@ -24,5 +24,12 @@ return {
 
         -- search help menus
         vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+
+        require("telescope").setup({
+            defaults = {
+                file_ignore_patterns = { "^.git/" }
+            }
+
+        })
     end
 }
