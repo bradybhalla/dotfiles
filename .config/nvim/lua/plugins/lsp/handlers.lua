37195e90476d1cbd9d1fc7e6b624621ae67f4bed
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2023-08-08 16:50:50 -0700

    lsp shortcuts specific to buffers

--- .config/nvim/lua/plugins/lsp/handlers.lua
+++ .config/nvim/lua/plugins/lsp/handlers.lua
@@ -5,32 +5,43 @@ local on_attach = function(client, bufnr)
 
     local builtin = require("telescope.builtin")
 
-    vim.keymap.set("n", "<leader>l", "<leader>l", { desc = "LSP prefix" })
+    vim.keymap.set("n", "<leader>l", "<leader>l", { desc = "LSP prefix", buffer = true })
 
-    vim.keymap.set("n", "<leader>lF", vim.lsp.buf.format, { desc = "format file" })
+    if client.server_capabilities.documentFormattingProvider then
+        vim.keymap.set("n", "<leader>lF", vim.lsp.buf.format, { desc = "format file", buffer = true })
+    end
 
-    vim.keymap.set("n", "<leader>ld", builtin.lsp_definitions, { desc = "go to definition" })
-    vim.keymap.set("n", "<leader>lr", builtin.lsp_references, { desc = "find references" })
+    if client.server_capabilities.definitionProvider then
+        vim.keymap.set("n", "<leader>ld", builtin.lsp_definitions, { desc = "go to definition", buffer = true })
+    end
 
-    vim.keymap.set("n", "<leader>lS", function()
-        -- some LSPs need inital query
-        vim.ui.input({ prompt = "Initial query: " }, function(query)
-            builtin.lsp_workspace_symbols({ query = query })
-        end)
-    end, { desc = "search symbols" })
+    if client.server_capabilities.referencesProvider then
+        vim.keymap.set("n", "<leader>lr", builtin.lsp_references, { desc = "find references", buffer = true })
+    end
 
-    vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, { desc = "hover" })
+    if client.server_capabilities.workspaceSymbolProvider then
+        vim.keymap.set("n", "<leader>lS", builtin.lsp_dynamic_workspace_symbols,
+            { desc = "search symbols", buffer = true })
+    end
 
-    vim.keymap.set("n", "<leader>ln", vim.lsp.buf.rename, { desc = "rename" })
+    if client.server_capabilities.hoverProvider then
+        vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, { desc = "hover", buffer = true })
+    end
+
+    if client.server_capabilities.renameProvider then
+        vim.keymap.set("n", "<leader>ln", vim.lsp.buf.rename, { desc = "rename", buffer = true })
+    end
 
     -- help with function args
-    vim.keymap.set("n", "<leader>lk", vim.lsp.buf.signature_help, { desc = "signature help" })
+    if client.server_capabilities.signatureHelpProvider then
+        vim.keymap.set("n", "<leader>lk", vim.lsp.buf.signature_help, { desc = "signature help", buffer = true })
+    end
 
     -- open float with diagnostics
-    vim.keymap.set("n", "<leader>le", vim.diagnostic.open_float, { desc = "show diagnostic" })
+    vim.keymap.set("n", "<leader>le", vim.diagnostic.open_float, { desc = "show diagnostic", buffer = true })
 
-    vim.keymap.set("n", "<leader>l,", vim.diagnostic.goto_prev, { desc = "go to previous diagnostic" })
-    vim.keymap.set("n", "<leader>l;", vim.diagnostic.goto_next, { desc = "go to next diagnostic" })
+    vim.keymap.set("n", "<leader>l,", vim.diagnostic.goto_prev, { desc = "go to previous diagnostic", buffer = true })
+    vim.keymap.set("n", "<leader>l;", vim.diagnostic.goto_next, { desc = "go to next diagnostic", buffer = true })
 
 
     -- disable highlighting from lsp (copied from Eric)
