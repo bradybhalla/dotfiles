08f938f6dd9d8804067dae7a158b5e60f2bf1a63
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2023-11-24 13:17:17 -0700

    made commands for more compliated keymaps

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -1,3 +1,7 @@
+----------
+-- Setup -
+----------
+
 vim.api.nvim_create_user_command("SetupTreesitter", function()
     vim.cmd(":TSInstall c lua python json typescript javascript latex")
 end, { desc = "Treesitter setup" })
@@ -9,4 +13,36 @@ end, { desc = "Mason setup" })
 vim.api.nvim_create_user_command("Setup", function()
     vim.cmd(":SetupTreesitter")
     vim.cmd(":SetupMason")
-end, { desc = "Setup" })
+end, { desc = "setup" })
+
+
+-------------------
+-- Keymap helpers -
+-------------------
+
+vim.api.nvim_create_user_command("OpenFinder", function()
+    local file_path = vim.fn.expand("%")
+    if file_path ~= "" then
+        os.execute("open -R \"" .. file_path .. "\"")
+    else
+        os.execute("open \"" .. vim.fn.expand("%:p:h") .. "\"")
+    end
+end, { desc = "reveal in Finder" })
+
+vim.api.nvim_create_user_command("ToggleSpell", function()
+    vim.opt_local.spell = not vim.opt_local.spell._value
+end, { desc = "toggle spellcheck" })
+
+vim.api.nvim_create_user_command("Lazygit", function()
+    local dir = vim.fn.expand("%:h")
+    if vim.fn.isdirectory(dir) == 0 then
+        dir = "."
+    end
+    require("toggleterm.terminal").Terminal:new({
+        cmd = "lazygit",
+        hidden = true,
+        direction = "float",
+        dir = dir,
+        count = 99
+    }):toggle()
+end, { desc = "open lazygit" })
--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -27,28 +27,10 @@ vim.keymap.set("n", "<leader>w", "<CMD>w<CR>", { desc = "save" })
 -- use system clipboard register
 vim.keymap.set({ "n", "v" }, "<leader>c", "\"+", { desc = "use system clipboard" })
 
--- open directory in finder
-vim.keymap.set("n", "<leader>O", "<CMD>silent !open \"%:h\"<CR>", { desc = "open directory" })
-
--- toggle spellcheck
-vim.keymap.set("n", "<leader>S", function()
-    vim.opt_local.spell = not vim.opt_local.spell._value
-end, { desc = "toggle spellcheck" })
-
--- toggleterm popups
-vim.keymap.set("n", "<leader>G", function()
-    local dir = vim.fn.expand("%:h")
-    if vim.fn.isdirectory(dir) == 0 then
-        dir = "."
-    end
-    require("toggleterm.terminal").Terminal:new({
-        cmd = "lazygit",
-        hidden = true,
-        direction = "float",
-        dir = dir,
-        count = 99
-    }):toggle()
-end, { desc = "lazygit" })
+-- custom command shortcuts
+vim.keymap.set("n", "<leader>O", "<CMD>OpenFinder<CR>", { desc = "reveal in Finder" })
+vim.keymap.set("n", "<leader>S", "<CMD>ToggleSpell<CR>", { desc = "toggle spellcheck" })
+vim.keymap.set("n", "<leader>G", "<CMD>Lazygit<CR>", { desc = "open lazygit" })
 
 ----------------
 -- LSP/linting -
