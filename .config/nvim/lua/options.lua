5cc6a24286b3bcb13a32a2c437e5b39a21ae19c7
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2023-11-29 16:32:34 -0800

    signcolumn

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -2,6 +2,7 @@ vim.g.mapleader          = " "
 
 vim.opt.number           = true
 vim.opt.relativenumber   = true
+vim.opt.signcolumn       = "yes"
 
 vim.opt.tabstop          = 4
 vim.opt.softtabstop      = 4
@@ -20,4 +21,3 @@ vim.g.loaded_netrwPlugin = 1
 
 vim.opt.termguicolors    = true
 ----------------------------
-
