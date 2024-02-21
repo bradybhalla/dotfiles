9769aa8069526f5ce45df668efd5adc172543f22
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2024-02-21 02:26:14 -0800

    simplify completions

--- .config/nvim/after/ftplugin/tex.lua
+++ .config/nvim/after/ftplugin/tex.lua
@@ -1,46 +1,14 @@
 -- Latex config file
 
--- special completions
-require("cmp").setup.buffer {
-    formatting = {
-        format = function(entry, vim_item)
-            if entry.source.name == "omni" then
-                local cmp_info, cmp_symbol = (vim_item.menu):match("%[(.+)%] ?(.*)")
-
-                if cmp_info == nil then
-                    -- other
-                    vim_item.kind = "latex"
-                elseif #cmp_info == 1 then
-                    -- citation
-                    vim_item.kind = "citation"
-                    vim_item.menu = cmp_info
-                else
-                    local cmp_type, cmp_source = cmp_info:match("(.+): (.+)")
-
-                    if cmp_type == nil then
-                        -- other without source
-                        vim_item.kind = cmp_info
-                        vim_item.menu = ""
-                    elseif cmp_type == "cmd" then
-                        -- math symbols / commands
-                        vim_item.kind = cmp_symbol == "" and "cmd" or cmp_symbol
-                        vim_item.menu = cmp_source
-                    else
-                        -- other with source
-                        vim_item.kind = cmp_type
-                        vim_item.menu = cmp_source
-                    end
-                end
-            end
-
-            return vim_item
-        end,
-    },
-    sources = {
+-- vimtex completions (uses omni instead of lsp)
+local cmp = require("cmp")
+cmp.setup.buffer {
+    sources = cmp.config.sources({
         { name = "omni" },
-        { name = "ultisnips" },
+        { name = "ultisnips" }
+    }, {
         { name = "buffer" },
-    },
+    })
 }
 
 -- run treesitter parser (it doesn't run by default with highlight off)
