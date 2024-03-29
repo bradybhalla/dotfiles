b9b2367e7ca977cd418fef2f49ab529ac2e5579c
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2024-03-28 21:11:07 -0600

    remove tabline from nvim and change tmux prefix to <C-s>

--- .config/nvim/lua/plugins/status.lua
+++ .config/nvim/lua/plugins/status.lua
@@ -26,13 +26,8 @@ return {
                 lualine_z = {}
             },
             tabline = {
-                lualine_a = { {
-                    "tabs",
-                    max_length = 3 * vim.o.columns / 4,
-                    mode = 1,
-                    use_mode_colors = true
-                } },
-                lualine_b = {},
+                lualine_a = { "filename" },
+                lualine_b = { "location" },
                 lualine_c = {},
                 lualine_x = {},
                 lualine_y = { "diff" },
@@ -40,5 +35,4 @@ return {
             }
         }
     end
-
 }
