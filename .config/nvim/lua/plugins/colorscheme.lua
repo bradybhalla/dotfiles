7988b2d52afc74ba26018eb5b38eec641eff27b7
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2024-07-31 22:29:37 -0600

    update background

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,7 @@ return {
     priority = 1000,
     config = function()
         require("catppuccin").setup({
+            transparent_background = false,
             integrations = {
                 mason = true,
             },
